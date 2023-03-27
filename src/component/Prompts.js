6318d453a601b7650cd2d3697b79c1c86dbf43a6
MIO <91351393+Kinakopan@users.noreply.github.com>
2023-03-27 00:30:30 -0700

    fix propt&article save error, close popup func

--- src/component/Prompts.js
+++ src/component/Prompts.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import styles from '@/src/styles/Home.module.css'
-import data from "@/src/data/Prompts.json";
+import styles from '@/src/styles/Home.module.css';
+import data from '@/src/data/Prompts.json';
 
 export default function Prompts(props){
 
@@ -11,7 +11,7 @@ export default function Prompts(props){
   );
   const [author, setAuthor] = useState(data[selectedIndex]?.author);
   const [sentence, setSentence] = useState(data[selectedIndex]?.sentence);
-  const [savedPrompts, setSavedPrompts] = useState([]);
+  const [savedPrompts, setSelectPrompts] = useState([]);
   const [prompt, setPrompt] = useState('');
   const [barIsOpen, setBarIsOpen] = useState(false);
 
@@ -21,19 +21,16 @@ export default function Prompts(props){
     setSentence(data[selectedIndex]?.sentence);
   }
 
-  function showBtnHandler() {
-    if (barIsOpen) {
-      setBarIsOpen(false);
-    } else {
-      setBarIsOpen(true);
-    }
+  function onClose() {
+    setBarIsOpen(false);
+    props.onClose();
   }
 
   return (
     <div className={styles.wrapper_prompt}>
       <button
         className={styles.btn_popup_close}
-        onClick={showBtnHandler}>
+        onClick={props.onClose}>
           <span dangerouslySetInnerHTML={{__html: '<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 5L5 15" stroke="#fff" strokeWidth="2" strokeLinecap="round"/><path d="M5 5L15 15" stroke="#fff" strokeWidth="2" strokeLinecap="round"/></svg>'}} />
       </button>
 
@@ -52,7 +49,8 @@ export default function Prompts(props){
 
       <button
         className={styles.btn_popup_save}
-        onClick={() => props.setSavedPrompt(`${sentence}\n\n--${author}`)}>Select
+        onClick={() => props.setSelectPrompt(`${sentence}\n\n--${author}`)}>
+          Select
       </button>
     </div>
   </div>
